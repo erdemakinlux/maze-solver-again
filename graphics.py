63700a7c4c583aeadec4e7346a0fa5d6097bf9e4
erdemakinlux <erdem.akin.lux>
2025-05-24 12:45:19 +0200

    ch2 l1

--- graphics.py
+++ graphics.py
@@ -1,6 +1,9 @@
 from tkinter import *
 
 
+
+
+
 class Window:
     def __init__(self,width,height):
         self.__root = Tk()
@@ -19,14 +22,25 @@ class Window:
         while self.__running:
             self.redraw()
 
+    def draw_line(self,line, fill_color="black"):
+        line.draw(self.__canvas,fill_color)
+
     def close(self):
         self.__running = False
 
 
-def main():
-    win = Window(600,800)
-    win.wait_for_close()
 
 
-if __name__ == "__main__":
-    main()
+
+class Line():
+    def __init__(self,p1 , p2):
+        self.p1=p1
+        self.p2=p2
+    
+    def draw(self,canvas,fill_color="black"):
+        canvas.create_line(self.p1.x,self.p1.y,self.p2.x,self.p2.y,fill=fill_color,width=2)
+
+class Point():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
--- main.py
+++ main.py
@@ -1,34 +1,15 @@
 #import tkinter
 from tkinter import Tk, BOTH, Canvas
+from graphics import Window, Line, Point
 
 
-class Window():
-        def __init__(self,width,height):
-                self.__root = Tk()
-                self.__root.title = ("Maze Solver")
-                self.__canvas = Canvas(self.__root,bg = "white", height=height, width=width)
-                self.__canvas.pack(fill=BOTH, expand=1)
-                self.__running = False
-                self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        
-        def redraw(self):
-                self.__root.update_idletasks()
-                self.__root.update()
-                
-                
-        def wait_for_close(self):
-                self.__running = True
-                while self.__running:
-                        self.redraw()
-        
-        def close(self):
-                self.__running = False
-        
 
 
 def main():
-        main_window = Window(800,600)
-        main_window.wait_for_close()
+    win = Window(600,800)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
+    win.wait_for_close()
 
 if __name__ == "__main__":
         main()
