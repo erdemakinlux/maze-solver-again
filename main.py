8d81bcfc0c2b0fdf7bc52dfae2a2ada6d030aad6
erdemakinlux <erdem.akin.lux>
2025-05-27 12:47:55 +0200

    ch2 l3 again

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-#import tkinter
-from tkinter import Tk, BOTH, Canvas
-from graphics import Window, Line, Point
 
+from graphics import Window
+from cell import Cell
 
 
 
 def main():
     win = Window(600,800)
-    l = Line(Point(50, 50), Point(400, 400))
-    l2 = Line(Point(400, 400), Point(600, 600))
-    win.draw_line(l, "black")
-    win.draw_line(l2, "red")
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50,50,100,100)
+    
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 if __name__ == "__main__":
