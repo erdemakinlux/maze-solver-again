bab9ce1f3539b3c99cc2c756f91a79d45ee8320b
erdemakinlux <erdem.akin.lux>
2025-05-30 11:26:51 +0200

    ch3 l2

--- maze.py
+++ maze.py
@@ -1,47 +1,52 @@
-
-
 from cell import Cell
+import random
 import time
+
 
 class Maze:
     def __init__(
-            self,
-            x1,
-            y1,
-            num_rows,
-            num_cols,
-            cell_size_x,
-            cell_size_y,
-            win,
-            ):
-        self._x1=x1
-        self._y1=y1
-        self._num_rows=num_rows
-        self._num_cols=num_cols
-        self._cell_size_x=cell_size_x
-        self._cell_size_y=cell_size_y
-        self._win=win
-        rows, cols = (self._cell_size_x,  self._cell_size_y)
-        self.__cells = [[0]*cols]*rows
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win=None,
+    ):
+        self.__cells = []
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+
         self.__create_cells()
 
-    
     def __create_cells(self):
-        for x in range(0,self._num_rows):
-            for y in range(0,self._num_cols):
-                self.__cells[x][y]=Cell(self._win)
-                self.__draw_cell(x,y)
-                print(f"{x}{y} was created.")
+        for i in range(self.__num_cols):
+            col_cells = []
+            for j in range(self.__num_rows):
+                col_cells.append(Cell(self.__win))
+            self.__cells.append(col_cells)
+        for i in range(self.__num_cols):
+            for j in range(self.__num_rows):
+                self.__draw_cell(i, j)
 
-    
-    def __draw_cell(self, i , j):
-        x1 = self._x1  + self._cell_size_x * i
-        y1 = self._y1 + self._cell_size_y * j
-        x2 = x1 + self._cell_size_x
-        y2 = y1 + self._cell_size_y
-        self.__cells[i][j].draw(x1,y1,x2,y2)
-        self.animate()
+    def __draw_cell(self, i, j):
+        if self.__win is None:
+            return
+        x1 = self.__x1 + i * self.__cell_size_x
+        y1 = self.__y1 + j * self.__cell_size_y
+        x2 = x1 + self.__cell_size_x
+        y2 = y1 + self.__cell_size_y
+        self.__cells[i][j].draw(x1, y1, x2, y2)
+        self.__animate()
 
-    def animate(self):
-        self._win.redraw()
+    def __animate(self):
+        if self.__win is None:
+            return
+        self.__win.redraw()
         time.sleep(0.05)
